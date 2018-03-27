a5008ed1bd46ec9cff45ee58bcec216dc1ae23c1
Seb Boving <seb@google.com>
2018-03-27 20:58:21 +0000

    More GPU coverage from dogfood feedback.

--- test_build.R
+++ test_build.R
@@ -47,6 +47,19 @@ Library("kmcudaR")
 Library("bayesCL")
 if (expectGPU) {
   Library("gpuR")
+  detectGPUs()
+  listContexts()
+  set.seed(11111)
+  gpuA <- gpuMatrix(rnorm(262144), nrow = 512, ncol = 512)
+  gpuB <- gpuA %*% gpuA
+  as.numeric(gpuB[1, 1]) == (32.51897782759634 + 7.105427e-15) # very high precision
+}
+library(reticulate)
+lib_device <- reticulate::import(module = "tensorflow.python.client.device_lib", as = "lib_device")
+lib_device$list_local_devices()
+stopifnot(lib_device$list_local_devices()[[1]]$name == "/device:CPU:0")
+if (expectGPU) {
+  stopifnot(lib_device$list_local_devices()[[1]]$name == "/device:GPU:0")
 }
 
 testPlot1 <- ggplot(data.frame(x=1:10,y=runif(10))) + aes(x=x,y=y) + geom_line()
